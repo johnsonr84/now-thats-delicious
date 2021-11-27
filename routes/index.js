3615f93d3deb066f1c16fe241cca5236ee2abd7f
Your Name <robertcjohnson1984@gmail.com>
2021-11-27 14:47:46 -0700

    get stores api

--- routes/index.js
+++ routes/index.js
@@ -56,5 +56,5 @@ router.post('/account/reset/:token',
 */
 
 router.get('/api/search', catchErrors(storeController.searchStores));
-
+router.get('/api/stores/near', catchErrors(storeController.mapStores));
 module.exports = router;
