c60c7d0f8458752dc3e8183fa3d34a1521865b08
Your Name <robertcjohnson1984@gmail.com>
2021-11-27 12:03:40 -0700

    add login and register route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
@@ -24,5 +26,21 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+
+// 1. Validate the registration data
+// 2. register the user
+// 3. we need to log them in
+router.post('/register',
+  userController.validateRegister,
+  // we need to know about errors if 
+  // validation will be passed, but registration 
+  // will be failed in some reasons, e.g. second 
+  // registration with same email
+  catchErrors(userController.register),
+  authController.login
+);
 
 module.exports = router;
