a82a06478b469bd05937cdfdbad3880f51bc0408
Your Name <robertcjohnson1984@gmail.com>
2021-11-27 11:36:39 -0700

    add get tags

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,9 @@ router.post('/add/:id',
 );
 
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
-
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
+
+router.get('/tags', catchErrors(storeController.getStoresByTag));
+router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 module.exports = router;
