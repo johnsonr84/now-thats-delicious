1f493352342afbb9427c97cfb6a37582c2a181eb
Your Name <robertcjohnson1984@gmail.com>
2021-11-27 10:49:08 -0700

    add stores route

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
-router.get('/', storeController.homePage);
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
 router.post('/add', catchErrors(storeController.createStore));
+
 
 module.exports = router;
