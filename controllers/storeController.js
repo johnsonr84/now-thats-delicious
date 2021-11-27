22a6c23f47862d3ab58e1808d965ccf5a766b38e
Your Name <robertcjohnson1984@gmail.com>
2021-11-27 11:52:36 -0700

    add store tagQuery and Promises

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -83,7 +83,13 @@ exports.getStoreBySlug = async (req, res, next) => {
 };
 
 exports.getStoresByTag = async (req, res) => {
-  const tags = await Store.getTagsList();
   const tag = req.params.tag;
-  res.render('tag', { tags, title: 'Tags', tag });
+  const tagQuery = tag || { $exists: true, $ne: [] };
+
+  const tagsPromise = Store.getTagsList();
+  const storesPromise = Store.find({ tags: tagQuery });
+  const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
+
+
+  res.render('tag', { tags, title: 'Tags', tag, stores });
 };
