fd77c6e9f37df4106f0ebd528b28b60f80c56f87
Your Name <robertcjohnson1984@gmail.com>
2021-11-26 23:22:52 -0700

    add views and update routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
-// Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
-});
+router.get('/', storeController.homePage);
 
 module.exports = router;
