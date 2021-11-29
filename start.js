b64a28c8e7d5cab495de09b619144b40ff991f1d
Your Name <robertcjohnson1984@gmail.com>
2021-11-28 22:04:43 -0700

    update process.env

--- start.js
+++ start.js
@@ -19,7 +19,7 @@ require('./models/Review');
 
 // Start our app!
 const app = require('./app');
-app.set('port', process.env.PORT || 7777);
+app.set('port', process.env.PORT || 3000);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
